53492cf66b4baaff16bd66a41839d57cb9fa6a1f
Jonathan Calver <calver@cs.toronto.edu>
2024-10-15 03:07:07 -0400

    Added Starter files and readme for Phase 1

---
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -0,0 +1,43 @@
+package use_case.login;
+
+import entity.User;
+
+/**
+ * DAO for the Login Use Case.
+ */
+public interface LoginUserDataAccessInterface {
+
+    /**
+     * Checks if the given username exists.
+     * @param username the username to look for
+     * @return true if a user with the given username exists; false otherwise
+     */
+    boolean existsByName(String username);
+
+    /**
+     * Saves the user.
+     * @param user the user to save
+     */
+    void save(User user);
+
+    /**
+     * Returns the user with the given username.
+     * @param username the username to look up
+     * @return the user with the given username
+     */
+    User get(String username);
+
+    /**
+     * Returns the user with the given username.
+     * set the user with the given username
+     * @param name t
+     */
+    void setCurrentUser(String name);
+
+    /**
+     * Returns the user with the given username.
+     * get the user with the given username
+     * @return the user
+     */
+    String getCurrentUser();
+}
